183acc982c2923998013e3cdfd49e4efef3506ac
ChengzhouWu <71668362+chengzhouwu@users.noreply.github.com>
2022-12-14 21:51:01 -0600

    Add files via upload

---
+++ R/datasets.R
@@ -0,0 +1,8 @@
+#' \code{beta} contains a beta matrix. The data is generated using Illumina 450K platform.
+#'
+#' The \code{beta} dataset is a beta matrix
+#'
+#' \code{phenotype} is a phenotype dataset.
+#' @name beta
+#'
+NULL
